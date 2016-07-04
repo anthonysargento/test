734584eeed7f382fa9f7fd83ca20a840a65c2bfc
Strackel <Strackel@Strackel-PC>
2016-07-04 18:44:32 +0200

    commit dev2

--- TestApp/src/testapp/TestApp.java
+++ TestApp/src/testapp/TestApp.java
@@ -16,7 +16,7 @@ public class TestApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        // this is fucking dev2 branch
     }
     
 }
